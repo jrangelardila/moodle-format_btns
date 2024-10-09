9663018b14146a2c9d2fb3908ec1484037ff6ac2
Jhon Rangel <jrangelardila@gmail.com>
2024-10-08 19:20:27 -0500

    Cambio  a 4.5

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'format_btns';
-$plugin->release = '1.6';
-$plugin->version = 2024090700;
+$plugin->release = '1.7';
+$plugin->version = 2024100800;
 $plugin->requires = 2023042400;
 $plugin->maturity = MATURITY_STABLE;
